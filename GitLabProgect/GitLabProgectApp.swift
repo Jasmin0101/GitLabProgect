418b2bda7f8a343350ba15818b4af63556c6f731
Jasmina0101 <zasminalave@gmail.com>
2026-04-29 23:07:28 +1000

    Initial Commit

---
+++ GitLabProgect/GitLabProgectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitLabProgectApp.swift
+//  GitLabProgect
+//
+//  Created by Жасмина on 29.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct GitLabProgectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
